23050238d402e1837e4b3eb6cb61aa7189e83adc
chungho <chungho.song.582@gmail.com>
2016-09-14 12:06:45 -0700

    add each modules in /app file since Nginx has problem with bower_components folder. Need time to investigate

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
+current_dir = $(shell pwd)
+
 build:
 	docker build -f Dockerfile -t angular_client .
 
+build-dist:
+	docker build -f Dockerfile.dist -t angular_build .
+
 start:
 	docker run -d -p 80:80 --link node angular_client
+
+start-dist:
+	docker run -d -v $(current_dir)/app:/app/bower_components angular_build
 
 clean:
 	docker rm $$(docker ps -q -f status=exited)
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,7 @@ angular
     'angular-loading-bar',
   ])
   .config(['$stateProvider','$urlRouterProvider','$ocLazyLoadProvider',function ($stateProvider,$urlRouterProvider,$ocLazyLoadProvider) {
-    
+
     $ocLazyLoadProvider.config({
       debug:false,
       events:true,
@@ -42,34 +42,34 @@ angular
                 $ocLazyLoad.load(
                 {
                    name:'toggle-switch',
-                   files:["bower_components/angular-toggle-switch/angular-toggle-switch.min.js",
-                          "bower_components/angular-toggle-switch/angular-toggle-switch.css"
+                   files:["angular-toggle-switch/angular-toggle-switch.min.js",
+                          "angular-toggle-switch/angular-toggle-switch.css"
                       ]
                 }),
                 $ocLazyLoad.load(
                 {
                   name:'ngAnimate',
-                  files:['bower_components/angular-animate/angular-animate.js']
+                  files:['angular-animate/angular-animate.js']
                 })
                 $ocLazyLoad.load(
                 {
                   name:'ngCookies',
-                  files:['bower_components/angular-cookies/angular-cookies.js']
+                  files:['angular-cookies/angular-cookies.js']
                 })
                 $ocLazyLoad.load(
                 {
                   name:'ngResource',
-                  files:['bower_components/angular-resource/angular-resource.js']
+                  files:['angular-resource/angular-resource.js']
                 })
                 $ocLazyLoad.load(
                 {
                   name:'ngSanitize',
-                  files:['bower_components/angular-sanitize/angular-sanitize.js']
+                  files:['angular-sanitize/angular-sanitize.js']
                 })
                 $ocLazyLoad.load(
                 {
                   name:'ngTouch',
-                  files:['bower_components/angular-touch/angular-touch.js']
+                  files:['angular-touch/angular-touch.js']
                 })
             }
         }
@@ -92,7 +92,7 @@ angular
               ]
             })
           }
-        } 
+        }
       })
       .state('dashboard.form',{
         templateUrl:'views/form.html',
@@ -115,8 +115,8 @@ angular
             return $ocLazyLoad.load({
               name:'chart.js',
               files:[
-                'bower_components/angular-chart.js/dist/angular-chart.min.js',
-                'bower_components/angular-chart.js/dist/angular-chart.css'
+                'angular-chart.js/dist/angular-chart.min.js',
+                'angular-chart.js/dist/angular-chart.css'
               ]
             }),
             $ocLazyLoad.load({
@@ -155,5 +155,3 @@ angular
        url:'/grid'
    })
   }]);
-
-    
